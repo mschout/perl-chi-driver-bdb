35a77f3c0fc9f1df621cb84022c1b4f377beb6de
Michael Schout <mschout@gkg.net>
2018-06-13 11:04:55 -0500

    Minor style updates

--- lib/CHI/Driver/BerkeleyDB.pm
+++ lib/CHI/Driver/BerkeleyDB.pm
@@ -13,12 +13,17 @@ use Moose;
 
 extends 'CHI::Driver';
 
-has 'db'       => ( is => 'ro', lazy_build => 1 );
-has 'db_class' => ( is => 'ro', default    => 'BerkeleyDB::Hash' );
-has 'dir_create_mode' => ( is => 'ro', isa => 'Int', default => oct(775) );
-has 'env' => ( is => 'ro', lazy_build => 1 );
-has 'filename' => ( is => 'ro', init_arg => undef, lazy_build => 1 );
-has 'root_dir' => ( is => 'ro' );
+has db => (is => 'ro', lazy_build => 1);
+
+has db_class => (is => 'ro', default => 'BerkeleyDB::Hash');
+
+has dir_create_mode => (is => 'ro', isa => 'Int', default => oct(775));
+
+has env => (is => 'ro', lazy_build => 1);
+
+has filename => (is => 'ro', init_arg => undef, lazy_build => 1);
+
+has root_dir => (is => 'ro');
 
 sub _build_filename {
     my $self = shift;
@@ -29,16 +34,22 @@ sub _build_env {
     my $self = shift;
 
     my $root_dir = $self->root_dir;
-    die "must specify one of env or root_dir" if !defined($root_dir);
-    mkpath( $root_dir, 0, $self->dir_create_mode )
-      if !-d $root_dir;
+
+    unless (defined $root_dir) {
+        die "must specify one of env or root_dir";
+    }
+
+    unless (-d $root_dir) {
+        mkpath( $root_dir, 0, $self->dir_create_mode );
+    }
+
     my $env = BerkeleyDB::Env->new(
         '-Home'   => $self->root_dir,
         '-Config' => {},
-        '-Flags'  => DB_CREATE | DB_INIT_CDB | DB_INIT_MPOOL
-      )
+        '-Flags'  => DB_CREATE | DB_INIT_CDB | DB_INIT_MPOOL)
       or die sprintf( "cannot open Berkeley DB environment in '%s': %s",
         $root_dir, $BerkeleyDB::Error );
+
     return $env;
 }
 
@@ -49,37 +60,38 @@ sub _build_db {
     my $db       = $self->db_class->new(
         '-Filename' => $filename,
         '-Flags'    => DB_CREATE,
-        '-Env'      => $self->env
-      )
+        '-Env'      => $self->env)
       or die
       sprintf( "cannot open Berkeley DB file '%s' in environment '%s': %s",
         $filename, $self->root_dir, $BerkeleyDB::Error );
+
     return $db;
 }
 
 sub fetch {
-    my ( $self, $key ) = @_;
+    my ($self, $key) = @_;
 
     my $data;
+
     return ( $self->db->db_get( $key, $data ) == 0 ) ? $data : undef;
 }
 
 sub store {
-    my ( $self, $key, $data ) = @_;
+    my ($self, $key, $data) = @_;
 
     $self->db->db_put( $key, $data ) == 0
       or die $BerkeleyDB::Error;
 }
 
 sub remove {
-    my ( $self, $key ) = @_;
+    my ($self, $key) = @_;
 
     $self->db->db_del($key) == 0
       or die $BerkeleyDB::Error;
 }
 
 sub clear {
-    my ($self) = @_;
+    my $self = shift;
 
     my $count = 0;
     $self->db->truncate($count) == 0
@@ -87,24 +99,28 @@ sub clear {
 }
 
 sub get_keys {
-    my ($self) = @_;
+    my $self = shift;
 
     my @keys;
     my $cursor = $self->db->db_cursor();
-    my ( $key, $value ) = ( "", "" );
+    my ($key, $value) = ('', '');
+
     while ( $cursor->c_get( $key, $value, BerkeleyDB::DB_NEXT() ) == 0 ) {
         push( @keys, $key );
     }
+
     return @keys;
 }
 
 sub get_namespaces {
-    my ($self) = @_;
+    my $self = shift;
 
     my @contents = read_dir( $self->root_dir );
+
     my @namespaces =
       map { $self->unescape_for_filename( substr( $_, 0, -3 ) ) }
       grep { /\.db$/ } @contents;
+
     return @namespaces;
 }
 
@@ -118,12 +134,12 @@ __END__
 
 =head1 SYNOPSIS
 
-    use CHI;
+ use CHI;
 
-    my $cache = CHI->new(
-        driver     => 'BerkeleyDB',
-        root_dir   => '/path/to/cache/root'
-    );
+ my $cache = CHI->new(
+     driver     => 'BerkeleyDB',
+     root_dir   => '/path/to/cache/root'
+ );
 
 =head1 DESCRIPTION
 
--- t/lib/CHI/Driver/BerkeleyDB/Tests.pm
+++ t/lib/CHI/Driver/BerkeleyDB/Tests.pm
@@ -3,9 +3,10 @@ package CHI::Driver::BerkeleyDB::Tests;
 use strict;
 use warnings;
 
+use parent 'CHI::t::Driver';
+
 use CHI::Test;
-use File::Temp qw(tempdir);
-use base qw(CHI::t::Driver);
+use File::Temp 'tempdir';
 
 my $root_dir;
 
@@ -14,10 +15,12 @@ sub testing_driver_class { 'CHI::Driver::BerkeleyDB' }
 sub new_cache_options {
     my $self = shift;
 
-    $root_dir ||=
-      tempdir( "chi-driver-berkeleydb-XXXX", TMPDIR => 1, CLEANUP => 1 );
-    return ( $self->SUPER::new_cache_options(),
-        root_dir => "$root_dir/berkeleydb" );
+    $root_dir ||= tempdir("chi-driver-berkeleydb-XXXX", TMPDIR => 1, CLEANUP => 1);
+
+    return (
+        $self->SUPER::new_cache_options(),
+        root_dir => "$root_dir/berkeleydb"
+    );
 }
 
 1;
